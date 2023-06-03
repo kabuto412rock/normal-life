6231ff1816b862f39e4e4734f34a01e83ae214cf
zongjia <kabuto412rock@gmail.com>
2023-06-03 17:15:00 +0800

    test:查詢花費api的測試考慮參數使用`limit`,`offset`的情況

--- backend/server.test.js
+++ backend/server.test.js
@@ -7,7 +7,7 @@ const Cost = db.costs;
 const request = require('supertest');
 
 const payload = {
-  name: '蝦仁肉絲炒飯2',
+  name: '蝦仁肉絲炒飯',
   cash: 65,
   remark: '午餐'
 }
@@ -33,12 +33,60 @@ describe('查詢花費紀錄 [GET] '+API_COST_URL, () => {
       response = await request(app).get(API_COST_URL).set('Accept', 'application/json')
     })
 
-  test('無資料狀態下查資料成功', async () => { 
-    expect(response.status).toEqual(200)
-    expect(response.body.success).toEqual(true)
-    expect(response.body.costs).toHaveLength(0);
-  })
+    test('無資料狀態下查資料成功', async () => { 
+      expect(response.status).toEqual(200)
+      expect(response.body.success).toEqual(true)
+      expect(response.body.costs).toHaveLength(0);
+    })
+    
+    test('預填30筆資料查資料成功', async () => { 
+      const promises = [];
+      for(let i =0;i< 30;i++) {
+        promises.push(Cost.create({...payload}));
+      }
+      await Promise.all(promises)
 
+      let searchResponse1 = await request(app).get(API_COST_URL).set('Accept', 'application/json')
+      const costs = searchResponse1.body.costs;
+      
+      expect(searchResponse1.status).toEqual(200)
+      expect(searchResponse1.body.success).toEqual(true)
+      expect(costs).toHaveLength(30);
+      
+      for(let i=0;i<30;i++) {
+        expect(costs[i].createdAt).toMatch(/^(\d{4})-(0[1-9]|1[0-2])-([12]\d|0[1-9]|3[01])T([01]\d|2[0-3]):[0-5]\d:[0-5]\d(\.\d+)?(Z|[+-]([01]\d|2[0-3]):[0-5]\d)?$/)
+        expect(costs[i].updatedAt).toMatch(/^(\d{4})-(0[1-9]|1[0-2])-([12]\d|0[1-9]|3[01])T([01]\d|2[0-3]):[0-5]\d:[0-5]\d(\.\d+)?(Z|[+-]([01]\d|2[0-3]):[0-5]\d)?$/)
+        expect(costs[i]).toEqual(expect.objectContaining({
+          id: i+1,
+          ...payload,
+        }))
+      }
+    })
+
+    test('使用limit=15, offset=5查詢資料後第5~20筆, 預設排序是用id', async () => { 
+      const expectdLimit = 15;
+      const expectedOffset = 5;
+      const queryParamsStr = new URLSearchParams({
+        limit: expectdLimit,
+        offset: expectedOffset,
+      }).toString()
+      let searchResponse = await request(app).get(API_COST_URL+'?'+queryParamsStr).set('Accept', 'application/json')
+      expect(searchResponse.status).toEqual(200)
+      expect(searchResponse.body.success).toEqual(true)
+
+      const costs = searchResponse.body.costs;
+      expect(costs).toHaveLength(15);
+
+      for(let i=0;i<expectdLimit;i++) {
+        expect(costs[i].createdAt).toMatch(/^(\d{4})-(0[1-9]|1[0-2])-([12]\d|0[1-9]|3[01])T([01]\d|2[0-3]):[0-5]\d:[0-5]\d(\.\d+)?(Z|[+-]([01]\d|2[0-3]):[0-5]\d)?$/)
+        expect(costs[i].updatedAt).toMatch(/^(\d{4})-(0[1-9]|1[0-2])-([12]\d|0[1-9]|3[01])T([01]\d|2[0-3]):[0-5]\d:[0-5]\d(\.\d+)?(Z|[+-]([01]\d|2[0-3]):[0-5]\d)?$/)
+        expect(costs[i]).toEqual(expect.objectContaining({
+          id: i+expectedOffset+1,
+          ...payload,
+        }))
+      }
+    })
+    
  })
 
 describe('新增每日花費 [POST] /api/costs', () => { 
@@ -111,7 +159,7 @@ describe('更新cost紀錄 [PATCH] /api/costs/1', () => {
     expect(response.body.success).toEqual(1)
     expect(response.body.msg).toEqual(`The cost[${expectedId}] update success`)
 
-    const costRow = await  Cost.findOne({id: expectedId});
+    const costRow = await  Cost.findOne({where: {id: expectedId}});
     expect(costRow).toEqual(expect.objectContaining({
       id: 1,
       ...payload,
@@ -128,7 +176,7 @@ describe('更新cost紀錄 [PATCH] /api/costs/1', () => {
     expect(response.status).toEqual(200)
     expect(response.body.success).toEqual(1)
     expect(response.body.msg).toEqual(`The cost[${expectedId}] update success`)
-    const costRow = await  Cost.findOne({id: expectedId});
+    const costRow = await  Cost.findOne({where: {id: expectedId}});
     expect(costRow).toEqual(expect.objectContaining({
       id: expectedId,
       name: newName,
@@ -137,6 +185,6 @@ describe('更新cost紀錄 [PATCH] /api/costs/1', () => {
     }))
   }) 
   
-  
+
 })
 //  4. TODO:( ):添加現有API的測試(jest) - 查詢cost紀錄 需要可增加參數 limit, offset
