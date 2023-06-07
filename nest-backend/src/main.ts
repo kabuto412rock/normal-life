41f49719c031b8356da3bbdc9b43fc35ec195bc2
zongjia <kabuto412rock@gmail.com>
2023-06-07 23:08:25 +0800

    fix:修正後端未將前端host添加到白名單產生的CORS問題

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowlist = ['http://localhost:5173'];
+  app.enableCors({ origin: allowlist });
   await app.listen(3000);
 }
 bootstrap();
